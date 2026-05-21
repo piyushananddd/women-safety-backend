7708a3b36eab98106e7ab93d8f7e0e6a84a7e692
Piyush Anand <piyushanand550@gmail.com>
2026-05-21 21:49:46 +0530

    added sms alert

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const twilio = require("twilio");
+
+require("dotenv").config();
+
+const client = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 const cors = require("cors");
 require("dotenv").config();
 
@@ -23,30 +31,48 @@ const sosSchema = new mongoose.Schema({
 
 const SOS = mongoose.model("SOS", sosSchema);
 
+app.get("/", (req, res) => {
+  res.send("Backend Working")
+})
 app.post("/sos", async (req, res) => {
 
-    try {
+  try {
 
-        const { latitude, longitude } = req.body;
+    const { latitude, longitude } = req.body;
 
-        const newSOS = new SOS({
-            latitude,
-            longitude
-        });
+    const newSOS = new SOS({
+      latitude,
+      longitude
+    });
 
-        await newSOS.save();
+    await newSOS.save();
 
-        res.status(200).json({
-            message: "SOS Sent Successfully"
-        });
+    const locationLink = `https://maps.google.com/?q=${latitude},${longitude}`;
 
-    } catch (error) {
+    await client.messages.create({
+      body: `🚨 EMERGENCY SOS ALERT!\n\nUser needs help.\nLocation: ${locationLink}`,
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: process.env.MY_PHONE_NUMBER
+    });
 
-        res.status(500).json({
-            error: error.message
-        });
-    }
+    res.status(200).json({
+      message: "SOS Sent Successfully"
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
+
 });
+
+
+
 
 app.listen(5000, () => {
     console.log("Server running on port 5000");
